09dfbd1cece3895bd0c9734b86b53d4867530657
Dan Szabo <dansz44@hotmail.com>
2019-04-11 09:49:03 +0200

    Questions
    My solutions to the Prog1 Questions (at least the ones I was assigned)

---
+++ 2.py
@@ -0,0 +1,16 @@
+'''Objective: Convert a number in any base to the same number in another base'''
+
+def convertBase(ls, b1, b2):
+    return 0
+#----USER INPUT----#
+try:
+    ls = []
+    num = int(input('Enter a number: '))
+    while num != 0:
+        ls.append(num)
+        num = int(input('Enter a number: '))
+    b1 = int(input('Number is in base: '))
+    b2 = int(input('Desired base: '))
+    print(ls)
+except ValueError:
+    print('Invalid data entered :(')

8fead4d992c79e6684da74c3a8e80ae166912266
Daniel Szabo <dansz44@hotmail.com>
2019-04-13 13:36:35 +0200

    Finished

--- 9.py
+++ 9.py
@@ -1,1 +1,12 @@
 '''Objective: Find the largest prime factor of 600851475143'''
+
+import math
+
+n = 600851475143
+maxFactor = []
+for i in range(2, math.ceil(math.sqrt(n))): #From 2 -> square root of number
+    while n % i == 0:   #Checks if numbers are divisible by anything else
+        maxFactor.append(i) #If so, adds them to the list
+        n /= i
+
+print(maxFactor[-1])    #Ensures that only the max factor is returned, not other ones

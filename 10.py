fcacffb8a659da6a0f11422b3fe56faf220223f9
Daniel Szabo <dansz44@hotmail.com>
2019-04-17 21:04:13 +0200

    Finished w/o Annotations

--- 10.py
+++ 10.py
@@ -1,1 +1,11 @@
 '''Objective: Find the smallest positive number that is evenly divisible by all the numbers from 1-20'''
+
+import math as m
+
+def minPosNum():
+	res = 1
+	for i in range(1, 21):
+		res *= (i // m.gcd(i, res))
+	return res
+
+print(minPosNum())

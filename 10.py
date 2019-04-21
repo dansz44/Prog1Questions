584a13c842b9f13c45907c205d5ec39236120bc2
Daniel Szabo <dansz44@hotmail.com>
2019-04-21 19:44:02 +0200

    Finished

--- 10.py
+++ 10.py
@@ -4,8 +4,9 @@ import math as m
 
 def minPosNum():
 	res = 1
-	for i in range(1, 21):
-		res *= (i // m.gcd(i, res))
-	return res
+	for i in range(1, 21):	#Iterates through the range 1-20
+		x = (i // m.gcd(i, res)) #i floor divided by the greatest common divisor of i and res (which starts at 1)
+		res *= x #result is multiplied by result * x
+	return res	#returns solution at the end of for loop iteration
 
 print(minPosNum())

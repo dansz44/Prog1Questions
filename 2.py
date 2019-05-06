244ec324a6cb34d48a297380b15230fd8900b994
Daniel Szabo <dansz44@hotmail.com>
2019-05-06 22:15:02 +0200

    Finished
    Program should work perfectly now, calculations are accurate & meet question criteria. Exceptions are handled accordingly.

--- 2.py
+++ 2.py
@@ -1,34 +1,38 @@
-'''Restriction: Only numbers in up to base 36 can be converted'''
+'''Takes a list of numbers, a starting base, and a target base - then converts the numbers in the starting base
+    to a number in the target base'''
 
 def convertBase(ls, b1, b2):
-    '''Converts numbers -> base 10'''
+    '''Converts the list of numbers to base 10'''
     b10List = []
-    for i in range(0, len(ls)):
-        if ls[i] < 0:   #Checks if number is negative, then makes it positive
-            ls[i] = ls[i] * -1
-        try:    #Checks if number entered is within base bounds
-            b10num = int(str(ls[i]), b1)
-            b10List.append(b10num)
-        except ValueError: #Error lies here, for when the number is out of bounds of base
-            if ls[i] // 10 > b1:
-               b10List.append(0)
-            else:
-                b10List.append(ls[i] // 10)
-    '''Converts base 10 numbers -> desired base''' #This part functions normally (I think)
-    finalList = []
-    for i in range(0, len(b10List)):
-        while b10List[i]:
-            finalList.append(int(b10List[i] % b2))
+    for i in range(0, len(ls)): #Iterates through list of numbers
+        if ls[i] < 0:   #Converts number to positive if entered negative
+            ls[i] *= -1
+        b10num = int(str(ls[i]), b1)    #Converts number to base 10
+        b10List.append(b10num)
+    '''Converts the base 10 number list to the target base'''
+    targetList = []
+    for i in range(0, len(b10List)):    #Iterates through new base 10 list of numbers
+        while b10List[i]:   #Converts from base 10 -> target base using method found below
+            targetList.append(int(b10List[i] % b2))
             b10List[i] //= b2
-    return finalList[::-1]
+    return targetList[::-1]
+
 def main():
-    ls = []
-    num = int(input('Enter a number: '))
-    while num != 0:
-        ls.append(num)
-        num = int(input('Enter a number: '))
-    b1 = int(input('Number is in base: '))
-    b2 = int(input('Desired base: '))
-    print(convertBase(ls,b1,b2))
+    try:
+        ls = [int(x) for x in input('Enter the list of numbers: ').split()] #Creates a list out of entered numbers
+        b1 = int(input('Enter starting base: '))
+        b2 = int(input('Enter target base: '))
+
+        print('Original List:', ls)
+        print('New List:', convertBase(ls, b1, b2))
+    except ValueError:
+        print('ERROR: One or more of the numbers you entered exceeded the bounds of your starting base :(')
 
 main()
+
+'''Converting from B10 to target base::
+1. Divide the number by the base
+2. Take the remainder and append it ot the list
+3. Repeat division using whole number quotient we got from the previous division
+4. Continue division until quotient is 0 (but still append the remainder)
+5. Answer of the remainders read bottom-up'''
